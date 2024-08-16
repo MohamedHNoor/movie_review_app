0e014eec876650917831b84655a8b7663bb64dad
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-08-16 16:15:39 +0200

    Add new, create actions, and form, header and footer partials

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,33 @@
 class MoviesController < ApplicationController
-before_action :set_movie, except: [:index]
+before_action :set_movie, except: [:index, :new, :create]
   def index
     @movies = Movie.all
   end
 
   def show
-    # @movie = Movie.find(params[:id])
   end
 
   def edit
-    # @movie = Movie.find(params[:id])
   end
 
   def update
-    # @movie = Movie.find(params[:id])
     @movie.update(movie_params)
     redirect_to @movie
+  end
+
+   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, status: :see_other
   end
 
   private
