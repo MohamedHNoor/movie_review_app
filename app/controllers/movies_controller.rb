e23e5bae26a5b3d3fb6bc23b342a71a5b66d287f
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-08-16 11:30:27 +0200

    Add edit form and update action to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,30 @@
 class MoviesController < ApplicationController
-
+before_action :set_movie, except: [:index]
   def index
     @movies = Movie.all
   end
 
   def show
+    # @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    # @movie = Movie.find(params[:id])
+  end
+
+  def update
+    # @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
+  def set_movie
     @movie = Movie.find(params[:id])
   end
 
