4c1b06282ed7f57ce7666eee5f33c3b614e3ef56
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-08-19 15:54:21 +0200

    Implemented one_to_many nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
+  
   root "movies#index"
 
-  resources :movies
+  resources :movies do
+    resources :reviews
+  end
   
-  # get "movies" => "movies#index"
-  # get "movies/:id" => "movies#show", as: "movie"
-  # get "movies/:id/edit" => "movies#edit", as: "movie_edit"
-  # patch "movies/:id" => "movies#update"
 end
