bd6caed36165a628750315051f03b04dcdfada3d
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-08-26 20:25:54 +0200

    Add sign-in functionality, authentication and current_user

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   resources :users
   get "signup" => "users#new"
 
+  resource :session, only: [ :new, :create, :destroy ]
+  get "sign-in" => "sessions#new"
+
 end
